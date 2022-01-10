deac7057b83d5ff0988778ad97a6b535313d4406
EC2 Default User <ec2-user@ip-172-31-22-173.ec2.internal>
2022-01-10 23:24:09 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to show_book_path(@book.id)
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
@@ -28,14 +28,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-       redirect_to show_book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
     else
-      @book = Book.find(params[:id])
+       @book.update(book_params)
        render :edit
     end
-    flash[:notice] = "Book was successfully updated."
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get root :to => 'homes#top'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show' , as: 'show_book'
+  get 'books/:id' => 'books#show' , as: 'book'
   get 'books/:id/edit' => 'books#edit' , as: 'edit_book'
   patch 'books/:id' => 'books#update' , as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
